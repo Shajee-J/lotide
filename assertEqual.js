495369be574256c3d5f7c643c6674c30d9d5502e
Shajee-j <Shajeejm@gmail.com>
2023-06-26 21:05:53 -0400

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ " + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑" + " Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+
+
+//           test code:
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, -1);
+assertEqual("working", "working");
+
+

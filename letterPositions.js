e897d6761602ae2091a99d790840ed6d54b36b9f
Shajee-j <Shajeejm@gmail.com>
2023-06-29 08:58:05 -0400

    create letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,59 @@
+const eqArrays = function(actual, expected) {
+  if (actual.toString() === expected.toString()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}] `);
+  } else if (!eqArrays(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+  return '';
+};
+
+
+
+
+
+const letterPositions = function(sentence) {
+  let results = {};
+  // alternate method to isolating the letters in the string using for...of loop: // for (let letter of sentence){
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+    
+
+
+
+
+// test values:
+
+const trialObject =
+{
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+};
+
+console.log(assertArraysEqual(Object.values(letterPositions("lighthouse in the house")), Object.values(trialObject)));

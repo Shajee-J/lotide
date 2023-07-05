500ef2121dadd123874521742a21cf9fc3fe6d16
Shajee-j <Shajeejm@gmail.com>
2023-07-05 15:33:16 -0400

    refactor && add mocha testing

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -3,7 +3,8 @@ const assertEqual = require("../assertEqual");
 
 //           test code:
 
+assertEqual("working", "working");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, -1);
-assertEqual("working", "working");
+
